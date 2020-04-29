4319d07a4bb73efc4182fefa795fe2b5f0a94360
ricardo <ricardotvera.54@gmail.com>
2020-04-29 18:10:54 -0400

    added two more methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,10 +1,26 @@
 module Enumerable
     def my_each 
         i = 0
-        while index < self.count
+        while i < self.count
             yield(self[i])
             i = i + 1
         end
     end
 
+    def my_each_with_index
+        i = 0
+        while index < self.count
+            yield(self[i], i)
+            i = i + 1
+        end
+    end
 
+    def my_select
+        i = 0
+        to_change = []
+        while i < self.count
+            self[i]{|obj| to_change << obj if yield(obj)}
+            i++
+            to_change
+        end
+    end

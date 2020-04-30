6664b3cae323d25c27b094c7ad7fa7303362a4d9
ricardo <ricardotvera.54@gmail.com>
2020-04-30 02:03:37 -0400

    Update enumerables.rb

--- enumerables.rb
+++ enumerables.rb
@@ -2,16 +2,18 @@ module Enumerable
 # rubocop:disable Style/CaseEquality
     def my_each 
         i = 0
-        while i < self.count
-            yield(self[i])
+        arr = self
+        while i < arr.count
+            yield(arr[i])
             i = i + 1
         end
     end
 
     def my_each_with_index
         i = 0
-        while index < self.count
-            yield(self[i], i)
+        arr = self
+        while index < arr.count
+            yield(arr[i], i)
             i = i + 1
         end
     end
@@ -19,8 +21,9 @@ module Enumerable
     def my_select
         i = 0
         to_change = []
-        while i < self.count
-            to_change << self[i] if yield(self[i])
+        arr = self
+        while i < arr.count
+            to_change << arr[i] if yield(arr[i])
             i = i + 1
         end
         to_change
@@ -29,8 +32,9 @@ module Enumerable
     def my_any?
         i = 0
         to_change = false
-        while i < self.count
-            to_change = true if yield(self[i])
+        arr = self
+        while i < arr.count
+            to_change = true if yield(arr[i])
             i = i + 1
         end
         to_change
@@ -39,8 +43,9 @@ module Enumerable
     def my_all?
         i = 0
         to_change = true
-        while i < self.count
-            to_change = false unless yield(self[i])
+        arr = self
+        while i < arr.count
+            to_change = false unless yield(arr[i])
             i = i + 1
         end
         to_change
@@ -49,8 +54,9 @@ module Enumerable
     def my_none?
         i = 0
         to_change = true
-        while i < self.count
-            to_change = false if yield(self[i])
+        arr = self
+        while i < arr.count
+            to_change = false if yield(arr[i])
             i = i + 1
         end
         to_change
@@ -59,19 +65,21 @@ module Enumerable
     def my_map(the_proc=nil)
         i = 0
         to_change = []
-        while i < self.count
+        arr = self
+        while i < arr.count
             unless the_proc == nil
-            to_change << (the_proc.call self[i])
+            to_change << (the_proc.call arr[i])
             else
-            to_change << yield(self[i])
+            to_change << yield(arr[i])
         end
         to_change
     end
 
     def my_inject fr_value
         i = 0
-        while i < self.count
-            fr_value = yield(fr_value,self[i])  
+        arr = self
+        while i < arr.count
+            fr_value = yield(fr_value,arr[i])  
             i = i + 1
         end
         fr_value

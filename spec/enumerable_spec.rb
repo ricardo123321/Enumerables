68a7c51a0c6f2d2e532bcda05b8ec59c76757d58
ricardo <ricardotvera.54@gmail.com>
2020-06-09 13:39:47 -0700

    Update enumerable_spec.rb

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../enumerables.rb'
 
 describe Enumerable do
+
   describe '#my_each' do
     let(:arr_num) { [1, 2, 3, 4] }
     let(:arr_str) { %w[a b c] }
@@ -15,4 +16,22 @@ describe Enumerable do
       expect(arr_num.my_each).to be_an Enumerator
     end
   end
+
+  describe '#my_each_with_index' do
+    let(:arr_num) { [1, 2, 3, 4] }
+    let(:arr_str) { %w[a b c] }
+    let(:vowels) { %w[a e i o u] }
+    it 'Print all the numbers with index bigger than 2' do
+      expect { arr_num.my_each_with_index { |n,index| print [n,index] if n > 2 }}.to output('[3, 2][4, 3]').to_stdout
+    end 
+    it 'print works with its index that have vowels' do
+      expect { arr_str.my_each_with_index { |n,index| print [n,index] if vowels.include? n }}.to output('["a", 0]').to_stdout
+    end
+    it 'Return enumerator when no block is given' do
+      expect(arr_num.my_each).to be_an Enumerator
+    end
+  end
+
+  describe '#my_select' do
+     
 end

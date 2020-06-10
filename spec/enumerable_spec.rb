391a8fa2204c8a2a4d4786b0606493d080d01ca3
ricardo <ricardotvera.54@gmail.com>
2020-06-09 23:01:58 -0700

    Update enumerable_spec.rb

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -21,10 +21,10 @@ describe Enumerable do
     let(:arr_str) { %w[a b c] }
     let(:vowels) { %w[a e i o u] }
     it 'Print all the numbers with index bigger than 2' do
-      expect { arr_num.my_each_with_index { |n, index| print [n,index] if n > 2 } }.to output('[3, 2][4, 3]').to_stdout
+      expect { arr_num.my_each_with_index { |n, index| print [n, index] if n > 2 } }.to output('[3, 2][4, 3]').to_stdout
     end
-    it 'print works with its index that have vowels' do
-      expect { arr_str.my_each_with_index { |n, index| print [n,index] if vowels.include? n } }.to output('["a", 0]').to_stdout
+    it 'print works with its index that have an a' do
+      expect { arr_str.my_each_with_index { |n, index| print [n, index] if n == 'a' } }.to output('["a", 0]').to_stdout
     end
     it 'Return enumerator when no block is given' do
       expect(arr_num.my_each).to be_an Enumerator
@@ -49,21 +49,21 @@ describe Enumerable do
   describe '#my_all?' do
     let(:false_num) { [29, 32, 54, 30, 17] }
     let(:true_num) { [2, 4, 6, 12, 34] }
-    let(:true_str)  { %w[kkkr rnunrr ppolkf mmnd] }
+    let(:true_str) { %w[kkkr rnunrr ppolkf mmnd] }
     let(:vowels) { %w[a e i o u] }
     it 'every number is bigger than 20?' do
-      expect(false_num.my_all?{ |n| n > 20 }).to be(false)
+      expect(false_num.my_all? { |n| n > 20 }).to be(false)
     end
     it 'every number is even?' do
-      expect(false_num.my_all?{ |n|  (n%2).zero? }).to be(false)
+      expect(false_num.my_all? { |n| (n % 2).zero? }).to be(false)
     end
     it 'Select strings bigger than 3 letters' do
-        expect(true_str.my_all? { |str| str if str.length > 3}).to be(true)
+        expect(true_str.my_all? { |str| str if str.length > 3 }).to be(true)
     end
   end
 
   describe '#my_inject' do
-    let(:arr_num) {[2, 4, 6]}
+    let(:arr_num) { [2, 4, 6] }
     it 'returns the result of operation performed on each element in
     the array with given argument as parameter.' do
       expect(arr_num.my_inject(10) { |sym, item| sym + item }).to eql(22)
@@ -71,19 +71,19 @@ describe Enumerable do
   end
 
   describe '#my_map' do
-    let(:arr_num) {[1, 2, 3, 4, 5]}
-    let(:arr_str) {['dog', 'door', 'road', 'blade']}
+    let(:arr_num) { [1, 2, 3, 4, 5] }
+    let(:arr_str) { %w[dog door road blade] }
     it 'returns the modified array performed the operation in the block on each element' do
       expect(arr_num.my_map { |element| element * 2 }).to eql([2, 4, 6, 8, 10])
     end
     it 'returns the modified array performed the operation in the block on each element' do
-      expect(arr_str.my_map { |word| word + "?" }).to eql(['dog?', 'door?', 'road?', 'blade?'])
+      expect(arr_str.my_map { |word| word + '?' }).to eql(['dog?', 'door?', 'road?', 'blade?'])
     end
   end
 
   describe '#my_none?' do
-    let(:arr_num) {[1, 2, 3, 4, 5]}
-    let(:arr_nums) {[2, 4, 6]}
+    let(:arr_num) { [1, 2, 3, 4, 5] }
+    let(:arr_nums) { [2, 4, 6] }
     it 'returns true if the condition does not hold for any elements' do
       expect(arr_nums.my_none?(String)).to eql(true)
     end
@@ -93,9 +93,9 @@ describe Enumerable do
   end
 
   describe '#my_any?' do
-    let(:arr_num) {[1, 2, 3, 4, 5]}
-    let(:arr_str) {['dog', 'door', 'road', 'blade']}
-    let(:arr_arg) {[2, nil, false]}
+    let(:arr_num) { [1, 2, 3, 4, 5] }
+    let(:arr_str) { %w[dog door road blade] }
+    let(:arr_arg) { [2, nil, false] }
     it "returns true if the condition holds for any elements" do
       expect(arr_num.my_any?(&:even?)).to eql(true)
     end
